b6c8d996f8e2aa1f60ec2b8a17c5332738bd7986
lipengsu <super2361@163.com>
2021-09-16 20:35:31 +0800

    多线程编程，创建线程的三种方式

---
+++ src/com/runoob/test/DisplayMessage.java
@@ -0,0 +1,15 @@
+package com.runoob.test;
+
+public class DisplayMessage implements Runnable {
+	   private String message;
+	   
+	   public DisplayMessage(String message) {
+	      this.message = message;
+	   }
+	   
+	   public void run() {
+	      while(true) {
+	         System.out.println(message);
+	      }
+	   }
+	}
---
+++ src/com/runoob/test/TestThread.java
@@ -0,0 +1,20 @@
+package com.runoob.test;
+
+public class TestThread {
+	 
+	   public static void main(String args[]) {
+//	      RunnableDemo R1 = new RunnableDemo( "Thread-1");
+//	      R1.start();
+//	      
+//	      RunnableDemo R2 = new RunnableDemo( "Thread-2");
+//	      R2.start();
+//	      
+	      System.out.println("------------------------------------");
+	      
+	      ThreadDemo T1 = new ThreadDemo( "Thread-3");
+	      T1.start();
+	      
+	      ThreadDemo T2 = new ThreadDemo( "Thread-4");
+	      T2.start();
+	   }   
+	}
