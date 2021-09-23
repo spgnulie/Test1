e492704d7353b6f67e7ddca3641eac1703f70b72
lipengsu <super2361@163.com>
2021-09-23 11:01:34 +0800

    更新数据库程序的操作

--- src/com/runoob/test/MySQLDemo.java
+++ src/com/runoob/test/MySQLDemo.java
@@ -5,15 +5,12 @@ import java.sql.*;
  
 public class MySQLDemo {
  
-    // MySQL 8.0 以下版本 - JDBC 驱动名及数据库 URL
-//    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-//    static final String DB_URL = "jdbc:mysql://localhost:3306/RUNOOB";
+	
+    // MySQL JDBC 驱动名及数据库 URL
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/java_sql";
  
-    // MySQL 8.0 以上版本 - JDBC 驱动名及数据库 URL
-    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";  
-    static final String DB_URL = "jdbc:mysql://localhost:3306/RUNOOB?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
- 
- 
+    
     // 数据库的用户名与密码，需要根据自己的设置
     static final String USER = "root";
     static final String PASS = "123456";
